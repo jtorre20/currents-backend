fd385db865a50994ab1349df10c7449ab4be27e0
jtorre20 <jtorres1989@gmail.com>
2018-05-16 11:43:32 -0400

    created association of top stories and section

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@
 
 #news_API_key = 8c9cb125d4414d3981669820dd0873d6
 
-# sections = Section.create([{title: "Top Stories"}, {title: "World"}, {title: "Science" }, {title: "U.S."}, {title: "Business"}, {title: "Technology"}])
 
 # top_stories = Story.create([{title: 'Fist-Pumping Jared Kushner Leaves Jerusalem Embassy Refreshed And Ready To Solve Next Global Crisis'}, { title: 'Trump Preemptively Tells Melania He Wont Give Her A Kidney'}])
 
@@ -16,13 +15,22 @@
 
 # Section.first.stories << top_stories
 # require 'pry'
-
+#******************HTTP REQUEST ****************************
 news_api = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=8c9cb125d4414d3981669820dd0873d6")
-# byebug
-news_data = JSON.parse(news_api)
+headlines_data = JSON.parse(news_api)
 
+#****************SECTIONS*************************
+sections = Section.create([{title: "Top Stories"}, {title: "World"}, {title: "Science" }, {title: "U.S."}, {title: "Business"}, {title: "Technology"}])
 
-news_data["articles"].each do |article| 
-  Story.create(title: article["title"], description: article["description"], url: article["url"], url_to_image: article["urlToImage"])
+top_stories = Section.all.find(id = 1)
+
+#***************STORIES***************************
+
+headline_articles = headlines_data["articles"].each do |article| 
+  story = Story.create(title: article["title"], description: article["description"], url: article["url"], url_to_image: article["urlToImage"])
+  #****************ASSOCIATION*********************
+  top_stories.stories << story
 end
+
+
 
